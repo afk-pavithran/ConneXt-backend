b430af4ee5255d63a326f74133a6731a11ca90a3
afk-pavithran <rupavithran@gmail.com>
2021-01-11 18:56:18 +0530

    user router

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //imports
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const { MONGO_URI } = require('./config');
-// const userRouter = require('./Routes/userRoutes');
+const userRouter = require('./Routes/userRoutes');
 
 
 
@@ -12,12 +12,12 @@ app.use(express.json())
 
 //register & login
 app.get('/', (req, res) => res.json('Deployed'))
-// app.use('/', userRouter)
+app.use('/', userRouter)
 
 
 
 
 app.listen(process.env.PORT || 4000, () => {
-    // mongoose.connect(process.env.MONGO_URI || MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
+    mongoose.connect(process.env.MONGO_URI || MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
     console.log('App and db connected')
 })
